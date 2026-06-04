1375fb9602cb61ba589a8bf575b89c4761829599
OmarSaadeldin <omarsaadeldin5909@gmail.com>
2026-06-05 00:01:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
   e.waitUntil(clients.claim());
 });
 
